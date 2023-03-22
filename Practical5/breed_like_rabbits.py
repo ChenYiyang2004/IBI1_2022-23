fa9ab562682cc15de94928b037c926470a844ffa
ChenYiyang2004 <127203800+ChenYiyang2004@users.noreply.github.com>
2023-03-22 16:56:49 +0800

    Update breed_like_rabbits.py

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -1,16 +1,15 @@
-#variable"born number" is the number of rabbits that will be born each generation
 #total number is 2 and generations is 1 at the beginning
-#While the total number of rabbits is no more than 100
-#	total number plus born number of this generation
-#	generations plus one
-#print the generations
-
 total_number=2
 generations=1
+#While the total number of rabbits is no more than 100
 while total_number<=100:
+	#variable"born number" is the number of rabbits that will be born each generation
 	born_number=2**generations
+	#	total number plus born number of this generation
 	total_number=total_number+born_number
+	#	generations plus one
 	generations=generations+1
+#print the generations
 a="Over 100 rabbits have been born untill "
 b=str(generations)
 c="th generation"
