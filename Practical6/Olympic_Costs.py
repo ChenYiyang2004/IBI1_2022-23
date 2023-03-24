bf86c33c914d263a0eef3bb4fbaf3d5f2cb3604a
ChenYiyang2004 <127203800+ChenYiyang2004@users.noreply.github.com>
2023-03-24 17:00:56 +0800

    new change in Olympic_Costs.py

--- Practical6/Olympic_Costs.py
+++ Practical6/Olympic_Costs.py
@@ -1,16 +1,17 @@
 #import matplotlib and name it as plt
 import matplotlib.pyplot as plt
-#input list of cost
-costs=[1,8,15,7,5,14,43,40]
-#sort the list
-costs.sort()
-#print the sorted list
+#input dictionary of cost
+costs={"Los Angeles 1984":1,"Seoul 1988":8,"Barcelona 1992":15,"Atlanta 1996":7,"Sydney 2000":5,"Athens 2003":14,"Beijing 2008":43,"London 2012":40}
+#sort the dictionary and make it become a list
+costs=sorted(costs.items(),key=lambda costs:costs[1])
+#change the list into dictionary
+costs=dict(costs)
+#print this dictionary
 print(costs)
 
 #draw bar plot of sorted costs and label it
-x=[1,2,3,4,5,6,7,8]
-labels=["Los Angeles 1984","Sydney 2000","Atlanta 1996","Seoul 1988","Athens 2003","Barcelona 1992","London 2012","Beijing 2008"]
-plt.xticks(x,labels,rotation=300)
-plt.bar(x,costs)
+x=[1,2,3,4,5,6,7,8] #x means the bar in x axis
+plt.xticks(x,costs.keys(),rotation=300)
+plt.bar(x,costs.values())
 #show the plot
 plt.show()
