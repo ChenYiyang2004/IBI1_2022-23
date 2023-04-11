04173254110218db3de927823baa014b81d59331
ChenYiyang2004 <Yiyang1.22@intl.zju.edu.cn>
2023-04-11 15:28:47 +0800

    add labels to plots

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -15,37 +15,51 @@ print(covid_data.iloc[0:1001:100,2])
 print(covid_data.loc[covid_data.loc[:,"location"]=="Afghanistan","total_cases"])
 #compute the mean number of new cases and new deaths on 31 March 2020
 new_data=covid_data.loc[covid_data.loc[:,"date"]=="2020-03-31",["location","new_cases","new_deaths"]]
-print(new_data)
 deaths=new_data.loc[:,"new_deaths"]
 d=np.mean(deaths)
 print(d)
 cases=new_data.loc[:,"new_cases"]
 c=np.mean(cases)
 print(c)
-#create boxplot of new cases and new deaths on 31 March 2020
+#create boxplot of new cases and new deaths on 31st March 2020
+plt.ylabel("number of cases")
+plt.xticks(deaths)
+plt.xlabel("new deaths in different areas")
+plt.title("New deaths on 31st March 2020")
 plt.boxplot(deaths)
 plt.show()
+plt.ylabel("number of cases")
+plt.xticks(cases)
+plt.xlabel("new cases in different areas")
+plt.title("New cases on 31st March 2020")
 plt.boxplot(cases)
 plt.show()
-#plot new cases and worldwide over time
+#plot new cases worldwide over time
 world=covid_data.loc[covid_data.loc[:,"location"]=="World",:]
 world_new_cases=world.loc[:,"new_cases"]
 world_dates=world.loc[:,"date"]
+plt.ylabel("number of cases")
+plt.xlabel("date")
+plt.title("New cases worldwide over time")
 plt.plot(world_dates, world_new_cases, 'b+')
-plt.xticks(world_dates.iloc[0:len(world_dates):4],rotation=-90)
+plt.xticks(world_dates.iloc[0:len(world_dates):4],rotation=300)
 plt.show()
 #plot new deaths worldwide over time
 world_new_deaths=world.loc[:,"new_deaths"]
+plt.ylabel("number of cases")
+plt.xlabel("date")
+plt.title("New deaths worldwide over time")
 plt.plot(world_dates, world_new_deaths, 'b+')
-plt.xticks(world_dates.iloc[0:len(world_dates):4],rotation=-90)
+plt.xticks(world_dates.iloc[0:len(world_dates):4],rotation=300)
 plt.show()
 #the code answering the question
-#show the plot of total cases and new cases in UK to show the development
+#show the plot of total cases in UK to show the development
 UK=covid_data.loc[covid_data.loc[:,"location"]=="United Kingdom",:]
 UK_total=UK.loc[:,"total_cases"]
-UK_new=UK.loc[:,"new_cases"]
 UK_date=UK.loc[:,"date"]
+plt.ylabel("number of cases")
+plt.xlabel("date")
+plt.title("Total cases in UK over time")
 plt.plot(UK_date,UK_total)
-plt.plot(UK_date,UK_new)
-plt.xticks(UK_date.iloc[0:len(UK_date):4],rotation=-90)
+plt.xticks(UK_date.iloc[0:len(UK_date):4],rotation=300)
 plt.show()
