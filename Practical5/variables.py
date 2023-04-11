3263581d3bd9d4079d11d65818c6775192e277f0
ChenYiyang2004 <127203800+ChenYiyang2004@users.noreply.github.com>
2023-04-11 14:13:35 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -4,10 +4,13 @@ c=116.39
 d=abs(b-a)
 e=abs(c-a)
 f= d<e
+print(f)
 #e is greater because f is True. Rob travelled further to Haining.
 
 X=True
 Y=False
 W=X and Y
 Z=X or Y
+print(W)
+print(Z)
 #W=False, Z=True
