5ad902b7fd5482b24658d44e4817fa008d9a6c8b
ChenYiyang2004 <127203800+ChenYiyang2004@users.noreply.github.com>
2023-04-11 14:36:18 +0800

    Update Favourite_movie_genres.py

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -1,15 +1,15 @@
 #import matplotlib and name it as ptl
-import matplotlib.pyplot as ptl
 #input the keys and values of dictionary "movie"
-movie={"Comedy":73,"Action":42,"Romance":38,"Fantasy":28,"Science-fiction":22,"Horror":19,"Crime":18,"Documentary":12,"History":8,"War":7}
 #print the dictionary
-print(movie)
 #draw pie plot using values in movie
-ptl.pie(x=movie.values(),labels=movie.keys())
 #show this plot
+#find the number that pairs with "requested_genre" and output as a sentence
+import matplotlib.pyplot as ptl
+movie={"Comedy":73,"Action":42,"Romance":38,"Fantasy":28,"Science-fiction":22,"Horror":19,"Crime":18,"Documentary":12,"History":8,"War":7}
+print(movie)
+ptl.pie(x=movie.values(),labels=movie.keys())
 ptl.show()
 # HERE: user can change the preferred genre:
 requested_genre="Comedy"
-#find the number that pairs with "requested_genre" and output as a sentence
 output="The number of students who likes "+requested_genre+" is "+str(movie[requested_genre])
 print(output)
