6fd4d0c8cab46b352ffb32261bb1f252a5fd6e06
ChenYiyang2004 <127203800+ChenYiyang2004@users.noreply.github.com>
2023-05-20 20:22:21 +0800

    Add files via upload

--- Practical14/autophagosome.py
+++ Practical14/autophagosome.py
@@ -4,13 +4,13 @@ import pandas
 DOMTree=xml.dom.minidom.parse("go_obo.xml")
 element=DOMTree.documentElement
 terms=element.getElementsByTagName("term")
-def childnodes(ID):
+def nodes(ID):
     node = 0
     for i in terms:
         is_a = i.getElementsByTagName('is_a')
         for j in is_a:
             if j.childNodes[0].nodeValue == ID:
-                node += childnodes(i.getElementsByTagName('id')[0].childNodes[0].nodeValue)+1
+                node += nodes(i.getElementsByTagName("id")[0].childNodes[0].nodeValue)+1
     return node
 data=[]
 for i in terms:
@@ -18,7 +18,7 @@ for i in terms:
 	if re.search("autophagosome",definition):
 		id=i.getElementsByTagName("id")[0].childNodes[0].nodeValue
 		name=i.getElementsByTagName("name")[0].childNodes[0].nodeValue
-		number=childnodes(i.getElementsByTagName("id")[0].childNodes[0].nodeValue)
+		number=nodes(i.getElementsByTagName("id")[0].childNodes[0].nodeValue)
 		data.append([id,name,definition,number])
 df=pandas.DataFrame(data,columns=["id","name","definition","childnodes"])
 df.to_excel("autophagosome.xlsx",index=False)
